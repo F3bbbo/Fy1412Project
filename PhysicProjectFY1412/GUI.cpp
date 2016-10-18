6f47106e412797aae22caefbe06d7e7af8a138cd
Mikael Johnson <Mick3_70@hotmail.com>
2016-10-18 10:31:57 +0200

    Fixed buttons
    Made new buttons for rotate and thrust, and placed buttons nearer each
    other.

--- PhysicProjectFY1412/GUI.cpp
+++ PhysicProjectFY1412/GUI.cpp
@@ -9,21 +9,22 @@ GUI::GUI()
 	nrOfButtons = 4;
 	buttons = new Button*[nrOfButtons];
 	//StartButton
-	buttons[0] = new StartButton("Textures\\gui\\StartButton.png");
+	buttons[0] = new StartButton("Textures\\gui\\ThrustButton.png");
 	buttons[0]->setSize(100, 20);
-	buttons[0]->setPosition(100, 100);
+	buttons[0]->setPosition(100, 200);
 	//RotationButtons
 	//RightButton
 	buttons[1] = new RotationButton("Textures\\gui\\RotationButton.png", 5);
 	buttons[1]->setSize(40, 40);
-	buttons[1]->setPosition(700, 400);
+	buttons[1]->setPosition(160, 300);
 	//LeftButton
 	buttons[2] = new RotationButton("Textures\\gui\\RotationButton.png", -5);
 	buttons[2]->setSize(40, 40);
-	buttons[2]->setPosition(650, 400);
+	buttons[2]->setPosition(100, 300);
 	//ResetButton
 	buttons[3] = new ResetButton();
-	buttons[3]->setPosition(100, 200);
+	buttons[3]->setSize(100, 20);
+	buttons[3]->setPosition(100, 250);
 
 
 }
